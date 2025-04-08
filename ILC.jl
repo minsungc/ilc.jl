3147b5816dcef6a7884e75e0e24fc77f4f838296
Minsung Cho <minsung@ccs.neu.edu>
2025-04-08 18:44:02 -0400

    haha

--- ILC.jl
+++ ILC.jl
@@ -3,8 +3,5 @@ module ILC
 using Match: @match
 
 include("src/syntax.jl")
-include("src/semantics.jl")
-
-export @term
 
 end
--- src/syntax.jl
+++ src/syntax.jl
@@ -1,106 +1,54 @@
-# Terms
+# Types
 
-abstract type Term end
+abstract type Type end
 
-const ValidConstTypes = Union{Int, String, Bool}
-
-mutable struct Var <: Term
+mutable struct TVar <: Type
     name::Symbol
-    typ::DataType
 end
 
-struct Const <: Term
-    val::ValidConstTypes
+struct TSum <: Type
+    inr::Type
+    inl::Type
 end
 
-struct Lambda <: Term
-    bound::Var
-    body::Term
+struct TProd <: Type
+    left::Type
+    right::Type
 end
 
-struct App <: Term
-    fn::Term
-    body::Term
+struct TMu <: Type
+    binder::Symbol
+    rest::Type
 end
 
-_var(s) = Var(s, Any)
-function _var(x::Symbol, y::Symbol)
-    @match y begin
-        :String => Var(x, String)
-        :Int    => Var(x, Int)
-        :Bool   => Var(x, Bool)
-    end
-end
-_const(x) = Const(x)
-_lam(x::Var, t) = Lambda(x, t)
-_lam(x::Symbol, t) = Lambda(_var(x),t)
-_app(f, x) = App(f, x)
+mk_var(e) = TVar(e)
+mk_tsum(l,r) = TSum(l,r)
+mk_tprod(l,r) = TProd(l,r)
+mk_tmu(b,e) = TMu(b,e)
 
-# Parser
-macro term(e)
-    return _expr_to_term(e)
+macro type(e)
+    return _expr_to_type(e)
 end
 
-function _expr_to_term(e)
-    if e isa Symbol return _var(e) end
-    if e isa ValidConstTypes return _const(e) end
+function _expr_to_type(e)
+    if e isa Symbol return mk_var(e) end
     @match e.head begin
-        :block  =>  return _expr_to_term(e.args[2])
-        :->     =>  return _lam(
-                        _expr_to_term(e.args[1]),
-                        _expr_to_term(e.args[2]),
-                    )
-        :(::)   =>  return  _var(e.args[1], e.args[2])
-        :call   =>  return _app(
-                        _expr_to_term(e.args[1]),
-                        _expr_to_term(e.args[2]),
-                    )
-    end
-end
-
-macro withtypes(e)
-    x = _expr_to_term(e)
-    return find_types(x, Vector{Var}())
-end
-
-const Env = Vector{Var}
-const empty_env = Vector{Var}()
-
-function is_valid_env(e::Env)
-    for v in e
-        if !(v.typ isa ValidConstTypes) return false end
-    end
-    return true
-end
-
-function find(x::Symbol, env::Env)
-    for e in env
-        if e.name == x return e end
-    end
-    return nothing
-end
-
-function find_types(x::Term, e::Env)
-    @match x begin
-        Var(s, t)       =>
-            begin
-                if t isa ValidConstTypes
-                    return x
-                elseif isnothing(find(s, e))
-                    return x
-                else
-                    return find(s,e)
-                end
+        :. => begin
+            b = e.args[1]
+            expr = e.args[2]
+            @match expr begin
+                ::QuoteNode => return mk_tmu(b, _expr_to_type(eval(expr))) # idk the parser's weird
+                ::Expr      => return mk_tmu(b, _expr_to_type(expr.args[1])) # idk the parser's weird
             end
-        Const(v)        =>
-            return x
-        Lambda(s, t)    =>
-            begin
-                news = find_types(s, e)
-                push!(e, news)
-                return Lambda(news, find_types(t, e))
+        end
+        :call => begin
+            op = e.args[1]
+            l = e.args[2]
+            r = e.args[3]
+            @match op begin
+                :+ => return mk_tsum(_expr_to_type(l), _expr_to_type(r))
+                :* => return mk_tprod(_expr_to_type(l), _expr_to_type(r))
             end
-        App(s, t)       =>
-            return App(find_types(s, e), find_types(t,e))
+        end
     end
 end
